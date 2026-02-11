ab27b49113bb228239345526a6b1d278a0607766
Ivan Pudev <ipudev@abv.bg>
2026-02-12 00:01:57 +0200

    Restructured viewModels folder

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizGame.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
